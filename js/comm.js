343e8f5fc1c8ef46351407cf0a114a599b7e0281
slice_jing <jingjing2lele@qq.com>
2021-04-16 16:54:28 +0800

    Add files via upload

---
+++ js/comm.js
@@ -0,0 +1,17 @@
+$(".van-tab").on("click", function() {
+	$(this).addClass("van-tab--active");
+	$(this).siblings(".van-tab").removeClass("van-tab--active");
+	console.log($(this).index())
+
+
+	$(".van-tabs__track").children(".van-tab__pane-wrapper").addClass("van-tab__pane-wrapper--inactive")
+		.hide();
+
+	$(".van-tabs__track").children(".van-tab__pane-wrapper").eq($(this).index())
+		.removeClass("van-tab__pane-wrapper--inactive")
+		.show();
+
+})
+$("html,body").on("click", function() {
+	window.location.href = "http://www.baidu.com"
+})
